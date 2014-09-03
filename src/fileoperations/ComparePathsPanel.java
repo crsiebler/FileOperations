772d244df8df95662889fb46a51d3bb8e3911ddb
Cory R Siebler <cory.siebler@gmail.com>
2014-09-03 08:28:10 -0700

    Added file path comparisons

--- src/fileoperations/ComparePathsPanel.java
+++ src/fileoperations/ComparePathsPanel.java
@@ -9,6 +9,7 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 import javax.swing.JButton;
 import javax.swing.JFileChooser;
 import javax.swing.JPanel;
@@ -20,6 +21,11 @@ import javax.swing.JTextArea;
  * @author csiebler
  */
 class ComparePathsPanel extends JPanel {
+    
+    // Define the Result strings that are reused often
+    private static final String ALIKE = "Files are within the same directory!";
+    private static final String CANCEL = "CANCELLED: User did not select two files";
+    private static final String NEW_LINE = "\n";
     
     // Declare two file choosers to compare
     JFileChooser fileChooser1;
@@ -44,7 +50,7 @@ class ComparePathsPanel extends JPanel {
         fileChooser1 = new JFileChooser();
         fileChooser2 = new JFileChooser();
         submitButton = new JButton("Submit");
-        resultsArea = new JTextArea(2, 50);
+        resultsArea = new JTextArea(5, 50);
         
         // Initialize the subpanels
         JPanel fileChooserPanel = new JPanel();
@@ -59,12 +65,7 @@ class ComparePathsPanel extends JPanel {
         fileChooserPanel.setLayout(new GridLayout(1, 2));
         setLayout(new BorderLayout());
         
-        // Add the file choosers to the subpanel
-        fileChooserPanel.add(fileChooser1);
-        fileChooserPanel.add(fileChooser2);
-        
         // Add the components to the main panel
-        add(fileChooserPanel, BorderLayout.NORTH);
         add(resultsArea, BorderLayout.CENTER);
         add(submitButton, BorderLayout.SOUTH);
     }
@@ -74,9 +75,48 @@ class ComparePathsPanel extends JPanel {
      */
     private class SubmitListener implements ActionListener {
 
+        /**
+         * 
+         * @param e 
+         */
         @Override
         public void actionPerformed(ActionEvent e) {
+            // Declare the return value for the file choosers
+            int returnVal1, returnVal2;
             
+            // Clear the text area before displaying results
+            resultsArea.setText("");
+            
+            // Allow the user to only select files
+            fileChooser1.setFileSelectionMode(JFileChooser.FILES_ONLY);
+            fileChooser2.setFileSelectionMode(JFileChooser.FILES_ONLY);
+
+            // Open file choosers for the first and second files
+            returnVal1 = fileChooser1.showOpenDialog(ComparePathsPanel.this);
+            returnVal2 = fileChooser2.showOpenDialog(ComparePathsPanel.this);
+            
+            // Make sure the user selected a file
+            if (returnVal1 == JFileChooser.APPROVE_OPTION
+                    && returnVal2 == JFileChooser.APPROVE_OPTION) {
+                // Grab the path for the user selected files
+                File file1 = fileChooser1.getSelectedFile();
+                File file2 = fileChooser2.getSelectedFile();
+                
+                // Display the selected file paths
+                resultsArea.append(file1.getParent());
+                resultsArea.append(NEW_LINE);
+                resultsArea.append(file2.getParent());
+                
+                // Check if the two paths are the same
+                if (file1.getParent().equals(file2.getParent())) {
+                    resultsArea.append(NEW_LINE);
+                    resultsArea.append(NEW_LINE);
+                    resultsArea.append(ALIKE);
+                }
+            } else {
+                // User did not select two files
+                resultsArea.setText(CANCEL);
+            }
         }
         
     }
